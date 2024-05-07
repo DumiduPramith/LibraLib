0a91251f93b85164d415ac1d028753ddaa9b15e9
DumiduPramith <dumidu42@gmail.com>
2024-05-07 23:52:41 +0530

    finalized GUI and Initial data

--- app/src/main/java/com/example/libralib/RegisterActivity.java
+++ app/src/main/java/com/example/libralib/RegisterActivity.java
@@ -33,12 +33,20 @@ public class RegisterActivity extends AppCompatActivity {
                 String email = edEmail.getText().toString();
                 String password = edPassword.getText().toString();
                 String confPassword = edConfirmPassword.getText().toString();
+
+                String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+                Database db = new Database(getApplicationContext(),"libralib",null,1);
+
                 if (username.length() == 0 || email.length() == 0 || password.length() == 0 || confPassword.length() == 0)
                     Toast.makeText(getApplicationContext(), "Please fill all the fields", Toast.LENGTH_SHORT).show();
-                else if (!password.equals(confPassword))
+                else if (!email.matches(emailPattern)) {
+                    Toast.makeText(getApplicationContext(), "Invalid email format", Toast.LENGTH_SHORT).show();
+                } else if (!password.equals(confPassword))
                     Toast.makeText(getApplicationContext(), "Password and Confirm Password do not match", Toast.LENGTH_SHORT).show();
                 else {
+                    db.register(username, email, password);
                     Toast.makeText(getApplicationContext(), "Registration Success", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
                 }
             }
         });
